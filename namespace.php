a58548832e01f2aa1f4a0af4950f07a5e6beae9c
gulnyr <nasvyaze@gmail.com>
2015-07-27 01:37:14 +0400

    Создали пространство имен

---
+++ namespace.php
@@ -0,0 +1,29 @@
+<?php
+
+// Пространству имен ничего не должно препятствовать,
+// даже html код и пробел
+// Это как-бы папки, но мы их представляем мысленно
+
+// Создаем название пространства имен
+
+namespace Test1;
+
+// Создадим тестовый класс Eddard
+
+class Eddard
+{
+    protected $string;
+    public function __construct($string) {
+        $this->string = $string;
+    }
+    public function strlen() {
+        return strlen($this->string);
+    }
+    public function show_str() {
+        echo $this->string . PHP_EOL;
+    }
+}
+
+//********************Продолжение на namespace2.php*************************
+
+?>
